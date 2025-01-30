5f3b9f6837c18181d770fe0e2f41c0b1887196b0
Arthur Lopez <arthur.lopez@outlook.com>
2025-01-30 09:38:44 +0100

    first commit

---
+++ draw_graph.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame({"x": np.array([1, 2, 3]), "y": np.array([2, 5, 8])})
+
+print(df)
+
+plt.plot(df["x"], df["y"])
+
+
+plt.show()
